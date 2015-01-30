1ce5e42492a5a8265c88581e5949cd869b3c319e
Kemal Alturk <kemal.ttt@gmail.com>
2015-01-30 05:11:15 -0800

    some changes

--- src/main/java/com/akifbatur/TerminalController.java
+++ src/main/java/com/akifbatur/TerminalController.java
@@ -62,7 +62,7 @@ public class TerminalController implements Serializable
 		}
 		else //Illegal command
 		{
-			System.out.println("illegal command");
+			System.out.println("you've made an illegal command");
 			//komut hakikaten çalışsın mı? o zaman burayı uncomment...
 //			StringBuffer output = new StringBuffer();
 //			 
